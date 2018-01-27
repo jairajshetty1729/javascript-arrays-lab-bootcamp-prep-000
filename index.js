fc3db27983589894803db27ddca8f5921802eb23
jairaj shetty <jayuchintu@gmail.com>
2018-01-27 06:18:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const app = "I don't do much."
-var kittens=[];
-function destructivelyAppendKitten(){
-  
+var kittens=["Milo", "Otis", "Garfield"];
+function destructivelyAppendKitten(name){
+  kittens.push('R');
 }
