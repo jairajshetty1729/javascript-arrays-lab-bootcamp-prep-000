8d4e2d27c5e65fc68ebfc4727e7b1dfb1d1d304a
jairaj shetty <jayuchintu@gmail.com>
2018-01-27 06:20:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ var kittens=["Milo", "Otis", "Garfield"];
 function destructivelyAppendKitten(name){
   kittens.push(name);
 }
-function destructivelyAppendKitten(name){
-  kittens.push(name);
+function destructivelyPrependKitten(name){
+  kittens.unshift(name);
+}
+function destructivelyRemoveLastKitten(name){
+  kittens.pop(name);
 }
